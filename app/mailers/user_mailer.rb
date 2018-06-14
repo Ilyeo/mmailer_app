a1f443ff2424fd7b5d8d573553519a0c6ca7569d
Ilyeo <jrag.tkd@gmail.com>
2018-06-14 14:02:21 -0500

    Render template from flexible way

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,10 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = 'http://email.com/login'
-    mail(to: @user.email, subject: 'Welcome to email app')
+    mail(to: @user.email,
+         subject: 'Welcome to email app') do |format|
+         format.html { render 'another_template' }
+         format.text { render 'welcome_email' }
+       end
   end
 end
