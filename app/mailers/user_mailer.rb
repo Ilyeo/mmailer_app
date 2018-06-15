c1cd10d13a33c1abca2fb7ef656340fcea88eeed
Ilyeo <jrag.tkd@gmail.com>
2018-06-14 19:33:41 -0500

    Add mjml-premailer gem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
   default from: 'notify@email.com'
+  layout 'mailer_layout'
 
   def welcome_email
     @user = params[:user]
     @url = 'http://email.com/login'
     mail(to: @user.email,
          subject: 'Welcome to email app') do |format|
-         format.html { render 'another_template' }
-         format.text { render 'welcome_email' }
+         format.html { render 'main', formats: [:mjml] }
        end
   end
 end
---
+++ config/initializers/mjml_premailer.rb
@@ -0,0 +1,7 @@
+MjmlPremailer.config.merge!(
+  debug:            false, # true/false
+  beautify:         true,  # true/false
+  minify:           false, # true/false
+  keep_comments:    false, # true/false
+  validation_level: :strict  # :strict/:soft/:skip
+)
